22f0d40109c29d752755571246a0fc84c8423c15
Yogie Kurniawan <yogieadekurniawan@gmail.com>
2023-12-20 11:58:44 +0700

    fix home page

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(
 // middleware to make 'user' available to all templates
 app.use(function (req, res, next) {
   res.locals.user = req.session.user;
+  console.log(res.locals.user);
   next();
 });
 // Parse JSON
